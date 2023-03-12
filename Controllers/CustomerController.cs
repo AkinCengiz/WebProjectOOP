77749766e44b32d32761eb67212623c4f099390a
akincengiz <akincengiz@outlook.com.tr>
2023-03-12 15:31:21 +0300

    Yeni dosyalar eklendi.

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebProjectOOP.Context;
+using WebProjectOOP.Entities;
+
+namespace WebProjectOOP.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private DbEntityContext context = new DbEntityContext();
+        public IActionResult Index()
+        {
+            var values = context.Customers.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddCustomer()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddCustomer(Customer customer)
+        {
+            if (customer.CustomerName.Length >= 6 && customer.CustomerCity.Length >= 3)
+            {
+                var addedCustomer = context.Entry(customer);
+                addedCustomer.State = EntityState.Added;
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+           
+        }
+        [HttpGet]
+        public IActionResult DeleteCustomer(int id)
+        {
+            var value = context.Customers.SingleOrDefault(c => c.CustomerID == id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteCustomer(Customer customer)
+        {
+            var deletedCustomer = context.Entry(customer);
+            deletedCustomer.State = EntityState.Deleted;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateCustomer(int id)
+        {
+            var value = context.Customers.SingleOrDefault(c => c.CustomerID == id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateCustomer(Customer customer)
+        {
+            var updatedCustomer = context.Entry(customer);
+            updatedCustomer.State = EntityState.Modified;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebProjectOOP.Context;
+using WebProjectOOP.Entities;
 
 namespace WebProjectOOP.Controllers
 {
@@ -10,6 +12,47 @@ namespace WebProjectOOP.Controllers
         {
             var values = entityContext.Products.ToList();
             return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct(Product product)
+        {
+            var addProducts = entityContext.Entry(product);
+            addProducts.State = EntityState.Added;
+            entityContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteProduct(int id)
+        {
+            var deleteProduct = entityContext.Products.SingleOrDefault(p => p.ProductId == id);
+            entityContext.Remove(deleteProduct);
+            //var deleteProduct = entityContext.Entry(product);
+            //deleteProduct.State = EntityState.Deleted;
+            entityContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateProduct(int id)
+        {
+            var value = entityContext.Products.SingleOrDefault(p => p.ProductId == id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateProduct(Product product)
+        {
+            var updatedProduct = entityContext.Entry(product);
+            updatedProduct.State = EntityState.Modified;
+            entityContext.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
